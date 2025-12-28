57b8fd512d305e9f8c515e12277b0a9a8047b499
melarkkkk <melark@192-168-100-126.lan>
2025-12-28 19:45:43 +0100

    feat: add additional tests

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -14,6 +14,13 @@ describe(`Function 'arrayReverse':`, () => {
     expect(res).toBeInstanceOf(Array);
   });
 
+  it('should return an empty array if the original one is empty', () => {
+    const array = [];
+    const res = [];
+
+    expect(arrayReverse(array)).toEqual(res);
+  });
+
   it(`should reverse an array
     of the words consists of Latin letters,
     numbers and special symbols`, () => {
@@ -23,25 +30,31 @@ describe(`Function 'arrayReverse':`, () => {
     const res2 = ['!22,', '2,1'];
     const array3 = ['QWErty', '#$%~^'];
     const res3 = ['^~%$#y', 'trEWQ'];
+    const array4 = ['Hell0'];
+    const res4 = ['0lleH'];
 
     expect(arrayReverse(array1)).toEqual(res1);
     expect(arrayReverse(array2)).toEqual(res2);
     expect(arrayReverse(array3)).toEqual(res3);
+    expect(arrayReverse(array4)).toEqual(res4);
   });
 
   it(`should keep the length of the strings from the original array`, () => {
-    const array = ['1', '22', '333', '4444'];
-    const res = ['4', '44', '433', '3221'];
+    const array1 = ['1', '22', '333', '4444'];
+    const res1 = ['4', '44', '433', '3221'];
+    const array2 = ['I', 'am', 'a', 'student!'];
+    const res2 = ['!', 'tn', 'e', 'dutsamaI'];
 
-    expect(arrayReverse(array)).toEqual(res);
+    expect(arrayReverse(array1)).toEqual(res1);
+    expect(arrayReverse(array2)).toEqual(res2);
   });
 
   it(`should return an empty string
     if original array consists of an empty string`, () => {
     const array1 = ['', 'Mate', '', 'academy'];
-    const array2 = ['', 'Mate', '', 'academy'];
+    const array2 = ['', '', ''];
     const res1 = ['', 'ymed', '', 'acaetaM'];
-    const res2 = ['', 'ymed', '', 'acaetaM'];
+    const res2 = ['', '', ''];
 
     expect(arrayReverse(array1)).toEqual(res1);
     expect(arrayReverse(array2)).toEqual(res2);
